21c48a48dac00e8b57f21c7a8f9e7476035e4268
RachaHachem <rachahachm@gmail.com>
2022-05-30 19:48:07 +0300

    commit 2

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', template_folder='templates')
 
 
 if __name__ == "__main__":
